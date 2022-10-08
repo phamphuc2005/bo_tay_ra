b537c1c9887e7c93f9dc784b8d4dcb8f9940c5ce
phamphuc2005 <minhpham2001bk@gmail.com>
2022-10-08 17:19:17 +0700

    Install dependencies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <h1>Don't touch your face</h1>
+    </div>
+  );
+}
+
+export default App;
